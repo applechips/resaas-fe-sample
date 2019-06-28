15bb8a3689211ce916e80b833a2ca702845fddba
Nicole <nhswong@gmail.comm>
2019-06-28 13:04:29 -0700

    refactor PostRow

--- src/PostRow.jsx
+++ src/PostRow.jsx
@@ -2,13 +2,9 @@ import React, { Component } from 'react'
 import { Card, Image } from 'semantic-ui-react'
 
 export default class Post extends Component {
-  constructor(props) {
-    super(props)
-  }
-
 
   render() {
-    const { user, post } = this.props
+    const { post, user} = this.props
 
     return (
       <Card className='post-card centered'>
@@ -20,14 +16,6 @@ export default class Post extends Component {
           <Card.Description>{post.body}</Card.Description>
         </Card.Content>
       </Card>
-      //   <div>
-      //     <img src="https://via.placeholder.com/120"/>
-      //     <p>{user.name}</p>
-      //     <p>{user.email} | {user.address.city}</p>
-      //   </div>
-      //   <p><strong>{post.title}</strong></p>
-      //   <p>{post.body}</p>
-      // </div>
     );
   }
 }
